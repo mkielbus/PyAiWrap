Fix colorfulness loss calculation

pyaiwrap/loss.py
@@ -577,14 +577,18 @@ class GeneratorColorizationLoss:
         colorfulnessOriginal = torch.tensor(0.0, device=reconstructed.device)
 
         if self.colorfulness_weight > 0:
-            # Convert to RGB for colorfulness calculation
             if self.target_channel == "ab" and reconstructed.shape[1] == 2:
-                if modified.shape[1] == 1:  # L channel
+                if modified.shape[1] == 3:  # RGB input
+                    lChannel = self.rgbToLuminance(modified)
+                    reconRgb = self.abToRgb(reconstructed, lChannel)
+                    originalRgb = self.abToRgb(original, lChannel)
+                elif modified.shape[1] == 1:  # Already L channel
                     reconRgb = self.abToRgb(reconstructed, modified)
                     originalRgb = self.abToRgb(original, modified)
                 else:
-                    reconRgb = self.convertToRgb(reconstructed, self.target_channel)
-                    originalRgb = self.convertToRgb(original, self.target_channel)
+                    fakeL = torch.ones_like(reconstructed[:, 0:1]) * 50.0
+                    reconRgb = self.abToRgb(reconstructed, fakeL)
+                    originalRgb = self.abToRgb(original, fakeL)
             else:
                 reconRgb = self.convertToRgb(reconstructed, self.target_channel)
                 originalRgb = self.convertToRgb(original, self.target_channel)